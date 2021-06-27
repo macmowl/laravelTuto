659ec20a3e1843f6ff880ba0f3e9e4ad6d498b7c
Alan Massaro <alan.massar@gmail.com>
2021-06-27 13:25:57 +0000

    Add brand form done

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Brand;
+use Illuminate\Support\Carbon;
 
 class BrandController extends Controller
 {
@@ -24,5 +25,17 @@ class BrandController extends Controller
         $brand_image = $request->file('brand_image');
         $name_gen = hexdec(uniqid());
         $img_extension = strtolower($brand_image->getClientOriginalExtension());
+        $img_name = $name_gen . '.' . $img_extension;
+        $up_location = 'images/brand/';
+        $last_img = $up_location.$img_name;
+        $brand_image->move($up_location, $last_img);
+
+        Brand::insert([
+            'brand_name' => $request->brand_name,
+            'brand_image' => $last_img,
+            'created_at' => Carbon::now()
+        ]);
+
+        return Redirect()->back()->with('succes', 'Brand successfully added');
     }
 }
